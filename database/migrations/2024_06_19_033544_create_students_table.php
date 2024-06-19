f84aa4942d07880f8953d08ad5410611fbd66e1e
imamrayhanekadinataa <99887562+imamrayhanekadinataa@users.noreply.github.com>
2024-06-19 11:31:56 +0700

    Add files via upload

---
+++ database/migrations/2024_06_19_033544_create_students_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id(); // Primary Key
+            $table->string('name'); // VARCHAR
+            $table->enum('class', ['9', '10', '11', '12']); // ENUM
+            $table->integer('status'); // INT
+            $table->timestamps(); // Created at and Updated at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
